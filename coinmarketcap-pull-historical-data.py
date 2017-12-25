4c1eac7bdbd52af6f88b6ccc920fbea159e5731c
pmohun <philmohun@gmail.com>
2017-12-25 11:13:50 -0500

    get_data function now iterates through all coins

--- coinmarketcap-pull-historical-data.py
+++ coinmarketcap-pull-historical-data.py
@@ -23,13 +23,15 @@ coins = [];
 for dict in json_data: # generate list of coins 
     coins.append(dict['id'])
 
+print(coins)
+
 ## Use list to pull historical data for each coin 
 end_date = str(date.today()).replace("-", "")
 base_url = "https://coinmarketcap.com/currencies/{0}/historical-data/?start=20130428&end=" + end_date ; 
 
 def get_data(coin):
-    print("Currency :", coins[0]) # todo change to 'coin'
-    url = base_url.format(coins[0])
+    print("Currency :", coin) # todo change to 'coin'
+    url = base_url.format(coin)
     html_response = requests.get(url).text.encode('utf-8')
     soup = BeautifulSoup(html_response, 'html.parser')
     table = soup.find_all('table')[0]
@@ -41,5 +43,6 @@ def get_data(coin):
         
 if __name__ == "__main__":
     for coin in coins:
+            print(coin)
             get_data(coin)
             pass  
